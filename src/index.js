811d7df8661a062829a0d30f322d8b284da8d425
Simbiat <ganiyusimbiat@gmail.com>
2019-02-12 15:38:40 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import myComponent from "./myComponent";
+
+ReactDOM.render(
+<div>
+  <myComponent/>
+</div>, document.getElementById('root'));
+
